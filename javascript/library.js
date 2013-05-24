42b5cc6062ce11a0e8fcc185d31c5d26a1264764
Benjamin Landers <benjaminrlanders@gmail.com>
2013-05-23 19:27:07 -0700

    renamed blog.js

---
+++ javascript/library.js
@@ -0,0 +1,12 @@
+function get(url) {
+   	return $.ajax({type: "GET", url: url, async: false}).responseText;
+}
+function addTo(name, msg){
+	$(name).html($(name).text()+msg);
+}
+function addToWithReturn(name, msg){
+	$(name).html($(name).html()+msg+"\n<br>\n");
+}
+function include(name){
+	$("div."+name).html(get("/"+name+".html"));
+}
