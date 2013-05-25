e517d311d2bd8c7f741e0b67b63c509ac686cf71
Benjamin Landers <benjaminrlanders@gmail.com>
2013-05-24 18:49:28 -0600

    removed faulty stuffs

--- javascript/library.js
+++ javascript/library.js
@@ -2,10 +2,10 @@ function get(url) {
    	return $.ajax({type: "GET", url: url, async: true}).responseText;
 }
 function get(url, funct) {
-   	$.ajax({type: "GET", url: url, async: true, success: funct(data)});
+   	$.ajax({type: "GET", url: url, async: true, success: funct});
 }
 function alt(url, funct) {
-   	$.ajax({type: "GET", url: url, async: true}).done(funct(data));
+   	$.ajax({type: "GET", url: url, async: true}).done(funct);
 }
 function addTo(name, msg){
 	$(name).html($(name).text()+msg);
